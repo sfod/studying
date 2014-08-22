04aaedb4d2afdcd5b56dcdcc656f30f1e0d77a07
sfod <octosyllabic@gmail.com>
2014-08-22 18:23:55 +0400

    [qt-board] add class EventData_NewGame

--- cpp/qt/board/events/event_data.hpp
+++ cpp/qt/board/events/event_data.hpp
@@ -1,10 +1,21 @@
 #ifndef EVENT_DATA_HPP
 #define EVENT_DATA_HPP
 
-class EventData
-{
+class EventData {
 public:
-    EventData();
+    explicit EventData(float ts = 0) : ts_(ts) {}
+    virtual ~EventData() {}
+
+    virtual float ts() const { return ts_; }
+
+private:
+    float ts_;
+};
+
+class EventData_NewGame : public EventData {
+public:
+    EventData_NewGame() {}
+    virtual ~EventData_NewGame() {}
 };
 
 #endif // EVENT_DATA_HPP
