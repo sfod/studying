d80f400243a345dcaa91890a2ab1fca0cf6b3bcb
sfod <octosyllabic@gmail.com>
2014-08-28 17:55:38 +0400

    [qt-board] add class CreatorComponent

---
+++ cpp/qt/board/actors/creator_component.cpp
@@ -0,0 +1,5 @@
+#include "creator_component.hpp"
+
+CreatorComponent::CreatorComponent()
+{
+}
---
+++ cpp/qt/board/actors/creator_component.hpp
@@ -0,0 +1,10 @@
+#ifndef CREATOR_COMPONENT_HPP
+#define CREATOR_COMPONENT_HPP
+
+class CreatorComponent : public ActorComponent
+{
+public:
+    CreatorComponent();
+};
+
+#endif // CREATOR_COMPONENT_HPP
