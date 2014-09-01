efb6c1f844d2f10a5ec9c89093458302f079b691
sfod <octosyllabic@gmail.com>
2014-09-01 17:41:12 +0400

    [qt-board] remove empty lines

--- cpp/qt/board/events/event_data.hpp
+++ cpp/qt/board/events/event_data.hpp
@@ -7,9 +7,7 @@ class EventData {
 public:
     explicit EventData(float ts = 0) : ts_(ts) {}
     virtual ~EventData() {}
-
     virtual const EventType &event_type() const = 0;
-
     virtual float ts() const { return ts_; }
 
 private:
@@ -20,9 +18,7 @@ class EventData_MainMenu : public EventData {
 public:
     EventData_MainMenu() : EventData() {}
     virtual ~EventData_MainMenu() {}
-
     virtual const EventType &event_type() const { return event_type_; }
-
     static const EventType event_type_;
 };
 
@@ -30,9 +26,7 @@ class EventData_NewGame : public EventData {
 public:
     EventData_NewGame() : EventData() {}
     virtual ~EventData_NewGame() {}
-
     virtual const EventType &event_type() const { return event_type_; }
-
     static const EventType event_type_;
 };
 
@@ -40,9 +34,7 @@ class EventData_Quit : public EventData {
 public:
     EventData_Quit() : EventData() {}
     virtual ~EventData_Quit() {}
-
     virtual const EventType &event_type() const { return event_type_; }
-
     static const EventType event_type_;
 };
 
