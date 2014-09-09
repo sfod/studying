b94256aac459e12220868a6f939549d0b6d986f7
sfod <octosyllabic@gmail.com>
2014-09-09 16:48:58 +0400

    [qt-board] remove hack with possible moves from GraphComponent

--- cpp/qt/board/actors/graph_component.cpp
+++ cpp/qt/board/actors/graph_component.cpp
@@ -20,10 +20,6 @@ bool GraphComponent::init(const boost::property_tree::ptree &component_data)
         auto it = (*pos).begin();
         pos_.first = it->second.get_value<int>();
         pos_.second = (++it)->second.get_value<int>();
-
-        possible_moves_.push_back(std::make_pair<int, int>(0, 3));
-        possible_moves_.push_back(std::make_pair<int, int>(1, 4));
-        possible_moves_.push_back(std::make_pair<int, int>(0, 5));
     }
     return true;
 }
