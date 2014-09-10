8f9944baab38986cc2ff80e30e5a6d99ade7e740
sfod <octosyllabic@gmail.com>
2014-09-10 17:07:11 +0400

    [qt-board] add class Node to represent pawn position

---
+++ cpp/qt/board/graph/node.cpp
@@ -0,0 +1,82 @@
+#include "node.hpp"
+#include <tuple>
+
+Node::Node() : row_(-1), col_(-1)
+{
+}
+
+Node::Node(int row, int col) : row_(row), col_(col)
+{
+}
+
+Node::Node(const Node &node)
+{
+    row_ = node.row_;
+    col_ = node.col_;
+}
+
+void Node::neighbours(int row_num, int col_num,
+        std::list<Node> *neighbours) const
+{
+    int n[3][3] = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    if (row_ - 1 < 0) {
+        n[0][0] = 0;
+        n[0][1] = 0;
+        n[0][2] = 0;
+    }
+    if (row_ + 1 >= row_num) {
+        n[2][0] = 0;
+        n[2][1] = 0;
+        n[2][2] = 0;
+    }
+    if (col_ - 1 < 0) {
+        n[0][0] = 0;
+        n[1][0] = 0;
+        n[2][0] = 0;
+    }
+    if (col_ + 1 >= col_num) {
+        n[0][2] = 0;
+        n[1][2] = 0;
+        n[2][2] = 0;
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (n[i][j] == 1) {
+                neighbours->push_back(Node(row_ + i - 1, col_ + j - 1));
+            }
+        }
+    }
+}
+
+bool Node::operator<(const Node &node) const
+{
+    return std::tie(row_, col_) < std::tie(node.row_, node.col_);
+}
+
+bool Node::operator==(const Node &node) const
+{
+    return (row_ == node.row_) && (col_ == node.col_);
+}
+
+bool Node::operator!=(const Node &node) const
+{
+    return !(*this == node);
+}
+
+const Node &Node::operator+=(const Node &node)
+{
+    row_ += node.row_;
+    col_ += node.col_;
+    return *this;
+}
+const Node Node::operator+(const Node &node) {
+    Node n = *this;
+    n += node;
+    return n;
+}
+
+std::ostream &operator<<(std::ostream &os, const Node &node)
+{
+    return os << node.row() << ":" << node.col();
+}
---
+++ cpp/qt/board/graph/node.hpp
@@ -0,0 +1,35 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <iostream>
+#include <list>
+
+class Node {
+public:
+    Node();
+    Node(int row, int col);
+    Node(const Node &node);
+
+    int row() const { return row_; }
+    int col() const { return col_; }
+
+    void set_row(int row) { row_ = row; }
+    void set_col(int col) { col_ = col; }
+
+    void neighbours(int row_num, int col_num,
+            std::list<Node> *neighbours) const;
+
+    bool operator<(const Node &node) const;
+    bool operator==(const Node &node) const;
+    bool operator!=(const Node &node) const;
+    const Node &operator+=(const Node &node);
+    const Node operator+(const Node &node);
+
+private:
+    int row_;
+    int col_;
+};
+
+std::ostream &operator<<(std::ostream &os, const Node &node);
+
+#endif // NODE_HPP
