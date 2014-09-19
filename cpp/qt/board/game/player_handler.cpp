d2a21307bf45fe39175d7d77f6e91a5b82924773
sfod <octosyllabic@gmail.com>
2014-09-19 16:43:11 +0400

    [qt-board] add class PlayerHandler

---
+++ cpp/qt/board/game/player_handler.cpp
@@ -0,0 +1,51 @@
+#include "player_handler.hpp"
+
+PlayerHandler::PlayerHandler() : player_list_(), current_player_(-1)
+{
+}
+
+PlayerHandler::~PlayerHandler()
+{
+}
+
+bool PlayerHandler::add_player(ActorId actor_id)
+{
+    player_list_.push_back(actor_id);
+    return true;
+}
+
+bool PlayerHandler::remove_player(ActorId actor_id)
+{
+    for (auto id : player_list_) {
+        if (id == actor_id) {
+            player_list_.remove(id);
+            return true;
+        }
+    }
+    return false;
+}
+
+ActorId PlayerHandler::next_player()
+{
+    auto it = player_list_.begin();
+    auto it_end = player_list_.end();
+
+    bool found = false;
+    for (; it != it_end; ++it) {
+        if ((*it) == current_player_) {
+            found = true;
+        }
+        else if (found) {
+            current_player_ = *it;
+            break;
+        }
+    }
+
+    // current player is not in player_list or at the end of player list,
+    // either way return the 1st player
+    if (it == it_end) {
+        current_player_ = *player_list_.begin();
+    }
+
+    return current_player_;
+}
---
+++ cpp/qt/board/game/player_handler.hpp
@@ -0,0 +1,23 @@
+#ifndef PLAYER_HANDLER_HPP
+#define PLAYER_HANDLER_HPP
+
+#include <list>
+#include "actors/actor.hpp"
+
+class PlayerHandler
+{
+public:
+    PlayerHandler();
+    virtual ~PlayerHandler();
+
+    bool add_player(ActorId actor_id);
+    bool remove_player(ActorId actor_id);
+    ActorId current_player() const { return current_player_; }
+    ActorId next_player();
+
+private:
+    std::list<ActorId> player_list_;
+    ActorId current_player_;
+};
+
+#endif // PLAYER_HANDLER_HPP
