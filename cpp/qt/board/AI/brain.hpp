343990ecea4a996a6341c3234189d355175feab5
sfod <octosyllabic@gmail.com>
2014-09-24 17:39:28 +0400

    [qt-board] add interface class Brain

---
+++ cpp/qt/board/AI/brain.hpp
@@ -0,0 +1,14 @@
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <memory>
+#include "graph/graph.hpp"
+
+class Brain {
+public:
+    virtual void make_move() = 0;
+    virtual void set_actor_id(ActorId actor_id) = 0;
+    virtual void set_graph(const std::shared_ptr<Graph> &graph) = 0;
+};
+
+#endif // BRAIN_HPP
