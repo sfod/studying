d9fe32f6e92471745d85374986db04b72840750f
sfod <octosyllabic@gmail.com>
2015-02-02 18:32:27 +0300

    [board] add method Brain::set_goal_nodes

--- cpp/qt/board/AI/brain.hpp
+++ cpp/qt/board/AI/brain.hpp
@@ -9,6 +9,7 @@ public:
     virtual void make_move() = 0;
     virtual void set_actor_id(ActorId actor_id) = 0;
     virtual void set_graph(const std::shared_ptr<Graph> &graph) = 0;
+    virtual void set_goal_nodes(const std::set<Node> &goal_nodes) = 0;
 };
 
 #endif // BRAIN_HPP
--- cpp/qt/board/AI/randomer_brain.hpp
+++ cpp/qt/board/AI/randomer_brain.hpp
@@ -12,6 +12,7 @@ public:
     virtual void make_move() override;
     virtual void set_actor_id(ActorId actor_id) { actor_id_ = actor_id; }
     virtual void set_graph(const std::shared_ptr<Graph> &graph) { graph_ = graph; }
+    virtual void set_goal_nodes(const std::set<Node> & /*goal_nodes*/) {}
 
 private:
     ActorId actor_id_;
